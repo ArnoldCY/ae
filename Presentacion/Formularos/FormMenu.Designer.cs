09aa6853308a93306b4788c275e6d1d5b0ba0a4b
ArnoldCY <152028384+ArnoldCY@users.noreply.github.com>
2023-11-29 00:21:33 -0400

    Add files via upload

---
+++ Presentacion/Formularos/FormMenu.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace Presentacion.Formularos
+{
+    partial class FormMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMenu));
+            this.PanelFormHijos = new System.Windows.Forms.Panel();
+            this.PanelMenu = new System.Windows.Forms.Panel();
+            this.btnCerrarSesion = new System.Windows.Forms.Button();
+            this.PanelEmpleado = new System.Windows.Forms.Panel();
+            this.btnCargo = new System.Windows.Forms.Button();
+            this.btnRegisEmpl = new System.Windows.Forms.Button();
+            this.btnEmpleado = new System.Windows.Forms.Button();
+            this.PanelArticulo = new System.Windows.Forms.Panel();
+            this.btnCategoria = new System.Windows.Forms.Button();
+            this.btnRegistrarA = new System.Windows.Forms.Button();
+            this.btnArticulo = new System.Windows.Forms.Button();
+            this.PanelSalida = new System.Windows.Forms.Panel();
+            this.btnReporteS = new System.Windows.Forms.Button();
+            this.btnRegistrarS = new System.Windows.Forms.Button();
+            this.btnSalida = new System.Windows.Forms.Button();
+            this.PanelEntrada = new System.Windows.Forms.Panel();
+            this.btnReporteE = new System.Windows.Forms.Button();
+            this.btnRegistrarE = new System.Windows.Forms.Button();
+            this.btnEntrada = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PanelMenu.SuspendLayout();
+            this.PanelEmpleado.SuspendLayout();
+            this.PanelArticulo.SuspendLayout();
+            this.PanelSalida.SuspendLayout();
+            this.PanelEntrada.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelFormHijos
+            // 
+            this.PanelFormHijos.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(189)))), ((int)(((byte)(91)))));
+            this.PanelFormHijos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.PanelFormHijos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelFormHijos.Location = new System.Drawing.Point(200, 0);
+            this.PanelFormHijos.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PanelFormHijos.Name = "PanelFormHijos";
+            this.PanelFormHijos.Size = new System.Drawing.Size(902, 655);
+            this.PanelFormHijos.TabIndex = 11;
+            // 
+            // PanelMenu
+            // 
+            this.PanelMenu.BackColor = System.Drawing.Color.Black;
+            this.PanelMenu.Controls.Add(this.btnCerrarSesion);
+            this.PanelMenu.Controls.Add(this.PanelEmpleado);
+            this.PanelMenu.Controls.Add(this.btnEmpleado);
+            this.PanelMenu.Controls.Add(this.PanelArticulo);
+            this.PanelMenu.Controls.Add(this.btnArticulo);
+            this.PanelMenu.Controls.Add(this.PanelSalida);
+            this.PanelMenu.Controls.Add(this.btnSalida);
+            this.PanelMenu.Controls.Add(this.PanelEntrada);
+            this.PanelMenu.Controls.Add(this.btnEntrada);
+            this.PanelMenu.Controls.Add(this.panel1);
+            this.PanelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelMenu.Location = new System.Drawing.Point(0, 0);
+            this.PanelMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PanelMenu.Name = "PanelMenu";
+            this.PanelMenu.Size = new System.Drawing.Size(200, 655);
+            this.PanelMenu.TabIndex = 10;
+            // 
+            // btnCerrarSesion
+            // 
+            this.btnCerrarSesion.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnCerrarSesion.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnCerrarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrarSesion.ForeColor = System.Drawing.Color.Black;
+            this.btnCerrarSesion.Location = new System.Drawing.Point(0, 614);
+            this.btnCerrarSesion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCerrarSesion.Name = "btnCerrarSesion";
+            this.btnCerrarSesion.Size = new System.Drawing.Size(200, 41);
+            this.btnCerrarSesion.TabIndex = 36;
+            this.btnCerrarSesion.Text = "Cerrrar sesion";
+            this.btnCerrarSesion.UseVisualStyleBackColor = false;
+            this.btnCerrarSesion.Click += new System.EventHandler(this.btnCerrarSesion_Click);
+            // 
+            // PanelEmpleado
+            // 
+            this.PanelEmpleado.Controls.Add(this.btnCargo);
+            this.PanelEmpleado.Controls.Add(this.btnRegisEmpl);
+            this.PanelEmpleado.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelEmpleado.Location = new System.Drawing.Point(0, 511);
+            this.PanelEmpleado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PanelEmpleado.Name = "PanelEmpleado";
+            this.PanelEmpleado.Size = new System.Drawing.Size(200, 89);
+            this.PanelEmpleado.TabIndex = 35;
+            this.PanelEmpleado.Visible = false;
+            // 
+            // btnCargo
+            // 
+            this.btnCargo.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnCargo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCargo.Location = new System.Drawing.Point(0, 44);
+            this.btnCargo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCargo.Name = "btnCargo";
+            this.btnCargo.Size = new System.Drawing.Size(200, 44);
+            this.btnCargo.TabIndex = 7;
+            this.btnCargo.Text = "Cargo";
+            this.btnCargo.UseVisualStyleBackColor = false;
+            // 
+            // btnRegisEmpl
+            // 
+            this.btnRegisEmpl.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnRegisEmpl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRegisEmpl.Location = new System.Drawing.Point(0, 0);
+            this.btnRegisEmpl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRegisEmpl.Name = "btnRegisEmpl";
+            this.btnRegisEmpl.Size = new System.Drawing.Size(200, 44);
+            this.btnRegisEmpl.TabIndex = 6;
+            this.btnRegisEmpl.Text = "Registar";
+            this.btnRegisEmpl.UseVisualStyleBackColor = false;
+            // 
+            // btnEmpleado
+            // 
+            this.btnEmpleado.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnEmpleado.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEmpleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmpleado.Location = new System.Drawing.Point(0, 467);
+            this.btnEmpleado.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEmpleado.Name = "btnEmpleado";
+            this.btnEmpleado.Size = new System.Drawing.Size(200, 44);
+            this.btnEmpleado.TabIndex = 34;
+            this.btnEmpleado.Text = "Empleado";
+            this.btnEmpleado.UseVisualStyleBackColor = false;
+            this.btnEmpleado.Click += new System.EventHandler(this.btnEmpleado_Click);
+            // 
+            // PanelArticulo
+            // 
+            this.PanelArticulo.Controls.Add(this.btnCategoria);
+            this.PanelArticulo.Controls.Add(this.btnRegistrarA);
+            this.PanelArticulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelArticulo.Location = new System.Drawing.Point(0, 377);
+            this.PanelArticulo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PanelArticulo.Name = "PanelArticulo";
+            this.PanelArticulo.Size = new System.Drawing.Size(200, 90);
+            this.PanelArticulo.TabIndex = 29;
+            this.PanelArticulo.Visible = false;
+            // 
+            // btnCategoria
+            // 
+            this.btnCategoria.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnCategoria.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCategoria.Location = new System.Drawing.Point(0, 44);
+            this.btnCategoria.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCategoria.Name = "btnCategoria";
+            this.btnCategoria.Size = new System.Drawing.Size(200, 44);
+            this.btnCategoria.TabIndex = 7;
+            this.btnCategoria.Text = "Categoria";
+            this.btnCategoria.UseVisualStyleBackColor = false;
+            // 
+            // btnRegistrarA
+            // 
+            this.btnRegistrarA.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnRegistrarA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRegistrarA.Location = new System.Drawing.Point(0, 0);
+            this.btnRegistrarA.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRegistrarA.Name = "btnRegistrarA";
+            this.btnRegistrarA.Size = new System.Drawing.Size(200, 44);
+            this.btnRegistrarA.TabIndex = 6;
+            this.btnRegistrarA.Text = "Registrar";
+            this.btnRegistrarA.UseVisualStyleBackColor = false;
+            // 
+            // btnArticulo
+            // 
+            this.btnArticulo.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnArticulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnArticulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnArticulo.Location = new System.Drawing.Point(0, 333);
+            this.btnArticulo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnArticulo.Name = "btnArticulo";
+            this.btnArticulo.Size = new System.Drawing.Size(200, 44);
+            this.btnArticulo.TabIndex = 28;
+            this.btnArticulo.Text = "Articulo";
+            this.btnArticulo.UseVisualStyleBackColor = false;
+            this.btnArticulo.Click += new System.EventHandler(this.btnArticulo_Click);
+            // 
+            // PanelSalida
+            // 
+            this.PanelSalida.Controls.Add(this.btnReporteS);
+            this.PanelSalida.Controls.Add(this.btnRegistrarS);
+            this.PanelSalida.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelSalida.Location = new System.Drawing.Point(0, 242);
+            this.PanelSalida.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PanelSalida.Name = "PanelSalida";
+            this.PanelSalida.Size = new System.Drawing.Size(200, 91);
+            this.PanelSalida.TabIndex = 27;
+            // 
+            // btnReporteS
+            // 
+            this.btnReporteS.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnReporteS.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReporteS.Location = new System.Drawing.Point(0, 44);
+            this.btnReporteS.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReporteS.Name = "btnReporteS";
+            this.btnReporteS.Size = new System.Drawing.Size(200, 44);
+            this.btnReporteS.TabIndex = 7;
+            this.btnReporteS.Text = "Reporte";
+            this.btnReporteS.UseVisualStyleBackColor = false;
+            // 
+            // btnRegistrarS
+            // 
+            this.btnRegistrarS.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnRegistrarS.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRegistrarS.Location = new System.Drawing.Point(0, 0);
+            this.btnRegistrarS.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRegistrarS.Name = "btnRegistrarS";
+            this.btnRegistrarS.Size = new System.Drawing.Size(200, 44);
+            this.btnRegistrarS.TabIndex = 6;
+            this.btnRegistrarS.Text = "Registrar";
+            this.btnRegistrarS.UseVisualStyleBackColor = false;
+            // 
+            // btnSalida
+            // 
+            this.btnSalida.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnSalida.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalida.Location = new System.Drawing.Point(0, 198);
+            this.btnSalida.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSalida.Name = "btnSalida";
+            this.btnSalida.Size = new System.Drawing.Size(200, 44);
+            this.btnSalida.TabIndex = 26;
+            this.btnSalida.Text = "Salida";
+            this.btnSalida.UseVisualStyleBackColor = false;
+            this.btnSalida.Click += new System.EventHandler(this.btnSalida_Click);
+            // 
+            // PanelEntrada
+            // 
+            this.PanelEntrada.Controls.Add(this.btnReporteE);
+            this.PanelEntrada.Controls.Add(this.btnRegistrarE);
+            this.PanelEntrada.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelEntrada.Location = new System.Drawing.Point(0, 108);
+            this.PanelEntrada.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.PanelEntrada.Name = "PanelEntrada";
+            this.PanelEntrada.Size = new System.Drawing.Size(200, 90);
+            this.PanelEntrada.TabIndex = 25;
+            this.PanelEntrada.Visible = false;
+            // 
+            // btnReporteE
+            // 
+            this.btnReporteE.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnReporteE.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnReporteE.Location = new System.Drawing.Point(0, 44);
+            this.btnReporteE.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReporteE.Name = "btnReporteE";
+            this.btnReporteE.Size = new System.Drawing.Size(200, 44);
+            this.btnReporteE.TabIndex = 7;
+            this.btnReporteE.Text = "Reportes";
+            this.btnReporteE.UseVisualStyleBackColor = false;
+            // 
+            // btnRegistrarE
+            // 
+            this.btnRegistrarE.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnRegistrarE.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRegistrarE.Location = new System.Drawing.Point(0, 0);
+            this.btnRegistrarE.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRegistrarE.Name = "btnRegistrarE";
+            this.btnRegistrarE.Size = new System.Drawing.Size(200, 44);
+            this.btnRegistrarE.TabIndex = 6;
+            this.btnRegistrarE.Text = "Registrar";
+            this.btnRegistrarE.UseVisualStyleBackColor = false;
+            // 
+            // btnEntrada
+            // 
+            this.btnEntrada.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnEntrada.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrada.Location = new System.Drawing.Point(0, 64);
+            this.btnEntrada.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEntrada.Name = "btnEntrada";
+            this.btnEntrada.Size = new System.Drawing.Size(200, 44);
+            this.btnEntrada.TabIndex = 10;
+            this.btnEntrada.Text = "Entrada";
+            this.btnEntrada.UseVisualStyleBackColor = false;
+            this.btnEntrada.Click += new System.EventHandler(this.btnEntrada_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 64);
+            this.panel1.TabIndex = 24;
+            // 
+            // FormMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 655);
+            this.Controls.Add(this.PanelFormHijos);
+            this.Controls.Add(this.PanelMenu);
+            this.Name = "FormMenu";
+            this.Text = "FormMenu";
+            this.PanelMenu.ResumeLayout(false);
+            this.PanelEmpleado.ResumeLayout(false);
+            this.PanelArticulo.ResumeLayout(false);
+            this.PanelSalida.ResumeLayout(false);
+            this.PanelEntrada.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelFormHijos;
+        private System.Windows.Forms.Panel PanelMenu;
+        private System.Windows.Forms.Button btnCerrarSesion;
+        private System.Windows.Forms.Panel PanelEmpleado;
+        private System.Windows.Forms.Button btnCargo;
+        private System.Windows.Forms.Button btnRegisEmpl;
+        private System.Windows.Forms.Button btnEmpleado;
+        private System.Windows.Forms.Panel PanelArticulo;
+        private System.Windows.Forms.Button btnCategoria;
+        private System.Windows.Forms.Button btnRegistrarA;
+        private System.Windows.Forms.Button btnArticulo;
+        private System.Windows.Forms.Panel PanelSalida;
+        private System.Windows.Forms.Button btnReporteS;
+        private System.Windows.Forms.Button btnRegistrarS;
+        private System.Windows.Forms.Button btnSalida;
+        private System.Windows.Forms.Panel PanelEntrada;
+        private System.Windows.Forms.Button btnReporteE;
+        private System.Windows.Forms.Button btnRegistrarE;
+        private System.Windows.Forms.Button btnEntrada;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ Presentacion/Formularos/FormReportSalida.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Presentacion.Formularos
+{
+    partial class FormReportSalida
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpFin = new System.Windows.Forms.DateTimePicker();
+            this.dtpInicio = new System.Windows.Forms.DateTimePicker();
+            this.dgvReporte = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporte)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtpFin
+            // 
+            this.dtpFin.Location = new System.Drawing.Point(330, 154);
+            this.dtpFin.Margin = new System.Windows.Forms.Padding(4);
+            this.dtpFin.MinDate = new System.DateTime(2023, 11, 28, 0, 0, 0, 0);
+            this.dtpFin.Name = "dtpFin";
+            this.dtpFin.Size = new System.Drawing.Size(265, 22);
+            this.dtpFin.TabIndex = 49;
+            // 
+            // dtpInicio
+            // 
+            this.dtpInicio.Location = new System.Drawing.Point(38, 154);
+            this.dtpInicio.Margin = new System.Windows.Forms.Padding(4);
+            this.dtpInicio.MinDate = new System.DateTime(2023, 11, 28, 0, 0, 0, 0);
+            this.dtpInicio.Name = "dtpInicio";
+            this.dtpInicio.Size = new System.Drawing.Size(265, 22);
+            this.dtpInicio.TabIndex = 48;
+            // 
+            // dgvReporte
+            // 
+            this.dgvReporte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReporte.Location = new System.Drawing.Point(38, 227);
+            this.dgvReporte.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvReporte.Name = "dgvReporte";
+            this.dgvReporte.RowHeadersWidth = 51;
+            this.dgvReporte.Size = new System.Drawing.Size(967, 324);
+            this.dgvReporte.TabIndex = 47;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 100);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 20);
+            this.label2.TabIndex = 46;
+            this.label2.Text = "Rango de fechas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(360, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 25);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "REPORTE SALIDA";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(818, 163);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(100, 28);
+            this.btnEliminar.TabIndex = 44;
+            this.btnEliminar.Text = "ELIMINAR";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Location = new System.Drawing.Point(679, 163);
+            this.btnRegistro.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(100, 28);
+            this.btnRegistro.TabIndex = 43;
+            this.btnRegistro.Text = "REGISTRO";
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            // 
+            // FormReportSalida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 592);
+            this.Controls.Add(this.dtpFin);
+            this.Controls.Add(this.dtpInicio);
+            this.Controls.Add(this.dgvReporte);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnRegistro);
+            this.Name = "FormReportSalida";
+            this.Text = "FormReportSalida";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporte)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpFin;
+        private System.Windows.Forms.DateTimePicker dtpInicio;
+        private System.Windows.Forms.DataGridView dgvReporte;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnRegistro;
+    }
+}
